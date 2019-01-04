a1e6bf98bba44389913e2f2e0d0882fecd2d2f25
Alexander Repeta <alexander.repeta@gmail.com>
2019-01-04 19:41:15 +0200

    Added boilerlate files

---
+++ src/js/utils/constants.js
@@ -0,0 +1,18 @@
+export const PRIORITY_TYPES = {
+  LOW: 0,
+  NORMAL: 1,
+  HIGH: 2,
+};
+
+export const NOTIFICATION_MESSAGES = {
+  NOTE_DELETED_SUCCESS: 'Заметка успешно удалена',
+  NOTE_ADDED_SUCCESS: 'Заметка успешно добавлена 🎉',
+  EDITOR_FIELDS_EMPTY: 'Заполните поля редактора',
+};
+
+export const NOTE_ACTIONS = {
+  DELETE: 'delete-note',
+  EDIT: 'edit-note',
+  INCREASE_PRIORITY: 'increase-priority',
+  DECREASE_PRIORITY: 'decrease-priority',
+};
