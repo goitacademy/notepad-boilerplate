bfe91c7e8ff8021550eb709eadd9096a66ab359d
Alexander Repeta <alexander.repeta@gmail.com>
2019-01-04 14:56:03 +0200

    Added boilerplate and updated readme

---
+++ src/js/utils/constants.js
@@ -0,0 +1,19 @@
+export const PRIORITY_TYPES = {
+  LOW: 0,
+  NORMAL: 1,
+  HIGH: 2,
+};
+
+export const ICON_TYPES = {
+  EDIT: 'edit',
+  DELETE: 'delete',
+  ARROW_DOWN: 'expand_more',
+  ARROW_UP: 'expand_less',
+};
+
+export const NOTE_ACTIONS = {
+  DELETE: 'delete-note',
+  EDIT: 'edit-note',
+  INCREASE_PRIORITY: 'increase-priority',
+  DECREASE_PRIORITY: 'decrease-priority',
+};
