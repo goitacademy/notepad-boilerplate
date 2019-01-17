cad989906c9a3c6dfd1e16d0d18d07aa204a5550
Alexander Repeta <alexander.repeta@gmail.com>
2019-01-17 17:22:01 +0200

    Adds webpack config

---
+++ build-utils/webpack.development.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = env => ({
+  devtool: 'cheap-module-source-map',
+  output: {
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html', minify: false })
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, '../src'),
+    watchContentBase: true,
+    publicPath: '/',
+    hot: false,
+    historyApiFallback: true,
+    open: false,
+    noInfo: true,
+    quiet: true,
+    stats: 'errors-only',
+    clientLogLevel: 'warning',
+    compress: true,
+    open: true,
+    port: env.port
+  }
+});
