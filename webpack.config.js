9f5bfa98d38bfc408553021c6f69d01fb335ff72
Alexander Repeta <axzerk@gmail.com>
2019-02-25 17:30:07 +0200

    Removed unused loader

--- webpack.config.js
+++ webpack.config.js
@@ -37,16 +37,6 @@ module.exports = ({ mode = 'production' }) =>
                   limit: 5000
                 }
               }
-              // {
-              //   loader: 'image-webpack-loader',
-              //   options: {
-              //     mozjpeg: { progressive: true, quality: 65 },
-              //     optipng: { enabled: false },
-              //     pngquant: { quality: '65-90', speed: 4 },
-              //     gifsicle: { interlaced: false },
-              //     webp: { quality: 75 },
-              //   },
-              // },
             ]
           }
         ]
